a603d7a65b655a56b53ba87512f2380c8f7816e4
dv-loginov <dilog@rambler.ru>
2022-11-02 13:36:21 +0300

    feat: add solution task 01-куread-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log(fs);
-console.log(path);
+console.log(path.join(__dirname, 'text.txt'));
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+stream.on('readable', () => {
+  const data = stream.read();
+  if (data != null) console.log(data);
+});
+// stream.on('end', () => console.log('Конец файла.'));
+stream.on('error', error => console.log('Error', error.message));
