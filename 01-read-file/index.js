447a570dc118ab17206e26db9124fc873e952e68
dv-loginov <dilog@rambler.ru>
2022-11-05 11:45:10 +0300

    refactor: removal of excess

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log(path.join(__dirname, 'text.txt'));
-
 const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
 stream.on('readable', () => {
   const data = stream.read();
   if (data != null) console.log(data);
 });
-// stream.on('end', () => console.log('Конец файла.'));
+
 stream.on('error', error => console.log('Error', error.message));
